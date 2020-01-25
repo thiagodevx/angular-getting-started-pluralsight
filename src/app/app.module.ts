30da0e5eb85dc6c1c890eeb4524b8378f2cd342b
thiago.dev.x <thiago.dev.x@gmail.com>
2020-01-25 17:36:41 -0300

    organization according to class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
