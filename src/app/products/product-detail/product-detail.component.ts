f3d7e096db0b6305f1bdb59ceb949d12d9c091f2
thiago.dev.x <thiago.dev.x@gmail.com>
2020-01-26 15:16:46 -0300

    creation of product detail properties and using the safe navigation operator with ?.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+
+  pageTitle: string;
+  product: Product;
 
   constructor() { }
 
