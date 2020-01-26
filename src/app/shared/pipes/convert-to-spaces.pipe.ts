9474a03953f5a6e4862e5c81e1df276fc6e676ba
thiago.dev.x <thiago.dev.x@gmail.com>
2020-01-25 21:51:33 -0300

    creating of the pipe, able to convert a character to spaces

--- src/app/shared/pipes/convert-to-spaces.pipe.ts
+++ src/app/shared/pipes/convert-to-spaces.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertToSpacesPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: string, character: string): string {
+    return value.replace(character, ' ');
   }
 
 }
