f010840040bc7ee2db234f374352526d64a8112a
thiago.dev.x <thiago.dev.x@gmail.com>
2020-01-26 18:45:44 -0300

    removal of unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
